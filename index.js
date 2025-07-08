7ef384e5fb1a48f70741b3da33e75cf762726f03
Theunknown4 <107306021+Theunknown4@users.noreply.github.com>
2025-07-08 13:50:06 +0200

    Finished js logic

---
+++ index.js
@@ -0,0 +1,59 @@
+let display = document.getElementById("display");
+
+/*numpad support */
+document.addEventListener("keydown", function(event) {
+    const key = event.key;
+    console.log(key);
+    if(!isNaN(key) || "+-*/.%".includes(key))
+    {
+        press(key);
+    }
+    else if (key === "Enter" || key === "=")
+    {
+        event.preventDefault();
+        calculate();
+    }
+    else if(key === "Backspace")
+    {
+        display.value = display.value.slice(0, -1);
+    }
+    else if(key === "Delete")
+    {
+        clearDisplay();
+    }
+})
+
+
+function press(value) {
+    if(!isNumOrSymbol(display.value) && display.value != "")
+    {
+        clearDisplay();
+        display.value += value;
+    }
+    else
+        display.value += value;
+}
+
+function calculate() {
+    if(display.value != "Error" && display.value != "")
+    {
+        try {
+            display.value = eval(display.value);
+        } catch {
+            display.value = "Error";
+        }
+    }
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function changeSign() {
+    console.log("WIP");
+}
+
+function isNumOrSymbol(str)
+{
+    return /^[0-9+.\-*\/%]+$/.test(str);
+}
